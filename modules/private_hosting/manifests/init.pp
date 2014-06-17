4602e68a972a781788312c7c6a96a50174c3a300
Dan Carley <dan.carley@gmail.com>
2014-06-17 08:40:51 +0100

    Private MySQL databases.

--- modules/private_hosting/manifests/init.pp
+++ modules/private_hosting/manifests/init.pp
@@ -1,6 +1,10 @@
 class private_hosting(
   $vhosts = {},
+  $databases = {},
 ) {
   validate_hash($vhosts)
   create_resources('nginx::resource::vhost', $vhosts)
+
+  validate_hash($databases)
+  create_resources('mysql::db', $databases)
 }
