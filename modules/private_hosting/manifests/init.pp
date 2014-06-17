62b0a7ceb67aa8ea8cf12ff45d70ae3152d2e6d7
Dan Carley <dan.carley@gmail.com>
2014-06-17 11:34:05 +0100

    Add private_hosting locations.

--- modules/private_hosting/manifests/init.pp
+++ modules/private_hosting/manifests/init.pp
@@ -1,9 +1,13 @@
 class private_hosting(
   $vhosts = {},
+  $locations = {},
   $databases = {},
 ) {
   validate_hash($vhosts)
   create_resources('nginx::resource::vhost', $vhosts)
+
+  validate_hash($locations)
+  create_resources('nginx::resource::location', $locations)
 
   validate_hash($databases)
   create_resources('mysql::db', $databases)
